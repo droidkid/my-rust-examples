a03344ae55d53eafdc4b197bce2e9f6a0308c8b1
Yuvaraj Chesetti <karch08@gmail.com>
2018-09-22 23:29:43 +0530

    Basic Window

--- game_of_life/src/main.rs
+++ game_of_life/src/main.rs
@@ -1,3 +1,19 @@
+extern crate sdl2;
+
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("rust sdl2", 800, 600)
+                                .position_centered()
+                                .opengl()
+                                .build()
+                                .unwrap();
+
+    thread::sleep(Duration::new(10, 0));
+
 }
