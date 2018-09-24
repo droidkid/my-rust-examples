400384e3d4187685a16d185812f46d0b94f9d855
Yuvaraj Chesetti <karch08@gmail.com>
2018-09-25 00:10:08 +0530

    Traits

--- game_of_life/src/main.rs
+++ game_of_life/src/main.rs
@@ -5,15 +5,35 @@ extern crate sdl2;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
-use sdl2::rect::Rect;
-use sdl2::render::{Canvas, RenderTarget};
 use sdl2::keyboard::Keycode;
 use std::thread;
 use std::time::Duration;
+use game_of_life::{GameOfLife, GameOfLifeRenderer};
+use renderable::{Renderable, Position2D};
+
+mod renderable {
+    extern crate sdl2;
+    use sdl2::render::{Canvas, RenderTarget};
+
+    pub struct Position2D{
+        pub x:i32, 
+        pub y:i32,
+    }
+
+    pub trait Renderable {
+        fn update(&mut self);
+        fn draw<T:RenderTarget>(&self, canvas: &mut Canvas<T>, pos:Position2D);
+    }
+
+}
 
 mod game_of_life {
 
     extern crate rand;
+    use sdl2::pixels::Color;
+    use sdl2::render::{Canvas, RenderTarget};
+    use sdl2::rect::Rect;
+    use renderable;
 
     #[derive(Copy, Clone,PartialEq)]
     pub enum CellState {
@@ -21,32 +41,21 @@ mod game_of_life {
         Dead,
     }
 
-    pub struct GameOfLife{
+    pub struct GameOfLife {
         rows: usize,
         cols: usize,
         board: Vec<Vec<CellState>>,
     }
 
+    pub struct GameOfLifeRenderer {
+        alive_color: Color,
+        dead_color: Color,
+        cell_width: u32,
+        cell_height: u32,
+        game_of_life: GameOfLife
+    }
+
     impl GameOfLife {
-
-        pub fn build_empty(rows:usize, cols:usize) -> GameOfLife {
-
-            let mut board: Vec<Vec<CellState>> = Vec::new();
-
-            for i in 0..rows {
-                board.push(Vec::new());
-
-                for _ in 0..cols {
-                    board[i].push(CellState::Dead);
-                }
-            }
-
-            GameOfLife {
-                rows,
-                cols,
-                board,
-            }
-        }
 
         pub fn build_random(rows:usize, cols:usize) -> GameOfLife {
 
@@ -70,7 +79,7 @@ mod game_of_life {
             }
         }
 
-        pub fn count_neighbours(&self, x:usize, y:usize) -> i32 {
+        fn count_neighbours(&self, x:usize, y:usize) -> i32 {
             let mut ret = 0;
             if x > 0 && self.board[x-1][y] == CellState::Alive {
                 ret = ret + 1;
@@ -133,37 +142,49 @@ mod game_of_life {
         pub fn cols(&self) -> usize {
             self.cols
         }
-
     }
-}
 
-fn draw<T: RenderTarget>(gameOfLife: &game_of_life::GameOfLife, canvas: &mut Canvas<T>, 
-                         sx:i32, sy:i32, cell_width:u32, cell_height:u32) {
 
-    let white = Color::RGB(255,255,255);
-    let black = Color::RGB(0,0,0);
-
-    for x in 0..gameOfLife.rows() {
-        for y in 0..gameOfLife.cols() {
-
-            let rx = (x as i32) * (cell_width as i32) + sx;
-            let ry = (y as i32) * (cell_height as i32) + sy;
-
-            let box_rect = Rect::new(rx, ry, cell_width, cell_height);
-
-            if gameOfLife.is_cell_alive(x, y) {
-                    canvas.set_draw_color(black);
+    impl GameOfLifeRenderer {
+        pub fn build(game_of_life:GameOfLife) -> GameOfLifeRenderer {
+            GameOfLifeRenderer {
+                dead_color : Color::RGB(255,255,255),
+                alive_color : Color::RGB(0,0,0),
+                cell_width : 20,
+                cell_height : 20,
+                game_of_life : game_of_life,
             }
-            else {
-                    canvas.set_draw_color(white);
-            }
-            canvas.fill_rect(box_rect);
-
         }
     }
 
-    canvas.present();
+    impl renderable::Renderable for GameOfLifeRenderer {
 
+        fn update(&mut self) {
+            self.game_of_life.update();
+        }
+
+        fn draw<T:RenderTarget>(&self, canvas: &mut Canvas<T>, pos: renderable::Position2D) {
+
+            for x in 0..self.game_of_life.rows() {
+                for y in 0..self.game_of_life.cols() {
+
+                    let rx = (x as i32) * (self.cell_width as i32) + pos.x;
+                    let ry = (y as i32) * (self.cell_height as i32) + pos.y;
+
+                    let box_rect = Rect::new(rx, ry, self.cell_width, self.cell_height);
+
+                    if self.game_of_life.is_cell_alive(x, y) {
+                        canvas.set_draw_color(self.alive_color);
+                    }
+                    else {
+                        canvas.set_draw_color(self.dead_color);
+                    }
+                    canvas.fill_rect(box_rect);
+                }
+            }
+            canvas.present();
+        }
+    }
 }
 
 
@@ -171,13 +192,10 @@ fn main() {
 
     let width = 800;
     let height = 600;
-    let box_width = 20;
-    let box_height = 20;
-    let num_box_rows = height / box_height;
-    let num_box_cols = width / box_width;
 
-    let mut gol1 = game_of_life::GameOfLife::build_random(20,20);
-    let mut gol2 = game_of_life::GameOfLife::build_random(20,20);
+    let mut gol1 = GameOfLifeRenderer::build(GameOfLife::build_random(20,20));
+    let mut gol2 = GameOfLifeRenderer::build(GameOfLife::build_random(20,20));
+
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -207,14 +225,14 @@ fn main() {
             }
         }
 
-        if (next_update < 0) {
+        if next_update < 0 {
             gol1.update();
             gol2.update();
             next_update = 100;
         }
 
-        draw(&gol1, &mut canvas, 0, 0, 10, 10);
-        draw(&gol2, &mut canvas, 300, 0, 10, 10);
+        gol1.draw(&mut canvas, Position2D{x:0, y:0});
+
 
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60)); // Update 60fps
         next_update = next_update - 1;
